b904f003c7eaf62b5bb93055f66b5cb305848191
shimizu-sh <shimizu@innocent.co.jp>
2019-01-23 15:06:21 +0900

    add stock_history test

--- stock_test.go
+++ stock_test.go
@@ -59,24 +59,108 @@ func TestStock(t *testing.T) {
 		t.Fatalf("Number of data should be 2. got=%d", len(stocks))
 	}
 
-	sto1 := Stock{
+	s1 := Stock{
 		StoreID:     json.Number("1"),
 		ProductID:   json.Number("1"),
 		StockAmount: "0",
 		UpdDateTime: "2018-09-25 12:50:50",
 	}
 
-	if !reflect.DeepEqual(stocks[0], &sto1) {
-		t.Fatalf("failed test expected: %v\n got: %v", sto1, *stocks[0])
+	if !reflect.DeepEqual(stocks[0], &s1) {
+		t.Fatalf("failed test expected: %v\n got: %v", s1, *stocks[0])
 	}
 
-	sto2 := Stock{
+	s2 := Stock{
 		StoreID:     json.Number("2"),
 		ProductID:   json.Number("1"),
 		StockAmount: "0",
 		UpdDateTime: "2018-07-28 12:38:42",
 	}
-	if !reflect.DeepEqual(stocks[1], &sto2) {
-		t.Fatalf("failed test expected: %v\n got: %v", sto2, *stocks[1])
+
+	if !reflect.DeepEqual(stocks[1], &s2) {
+		t.Fatalf("failed test expected: %v\n got: %v", s2, *stocks[1])
+	}
+}
+
+func TestStockHistory(t *testing.T) {
+	defer gock.Off()
+
+	dir, err := ioutil.TempDir("", "stock_history")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(dir) // clean up
+
+	gock.New("https://webapi.smaregi.jp").
+		Post("/access").
+		MatchHeader("X_contract_id", "(.*)").
+		MatchHeader("X_access_token", "(.*)").
+		MatchHeader("Content-Type", "application/x-www-form-urlencoded").
+		Reply(200).
+		File("./testdata/stock_history.json")
+
+	c := SrConfig{
+		ContractID:  "sumareji_id",
+		AccessToken: "sumareji_token",
+		EndPoint:    "https://webapi.smaregi.jp/access/",
+		OutputDir:   dir,
+		TableNames:  []string{STOCK_HISTORY},
+	}
+
+	err = Main(c)
+	if err != nil {
+		t.Fatalf("failed test %#v", err)
+	}
+
+	csvFile, err := os.OpenFile(path.Join(dir, "StockHistory.csv"), os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		t.Fatalf("failed test %#v", err)
+	}
+	defer csvFile.Close()
+
+	sht := []*StockHistory{}
+
+	if err := gocsv.UnmarshalFile(csvFile, &sht); err != nil { // Load clients from file
+		t.Fatalf("failed test %#v", err)
+	}
+
+	if len(sht) != 2 {
+		t.Fatalf("Number of data should be 2. got=%d", len(sht))
+	}
+
+	emptyJSONNumber := json.Number("")
+
+	s1 := StockHistory{
+		ID:             json.Number("1"),
+		UpdDateTime:    "2018-07-22 11:45:45",
+		TargetDateTime: "2018-07-22 11:45:45",
+		ProductID:      json.Number("4"),
+		StoreID:        json.Number("1"),
+		Amount:         json.Number("1"),
+		StockAmount:    json.Number("1"),
+		StockDivision:  "01",
+		FromStoreID:    &emptyJSONNumber,
+		ToStoreID:      &emptyJSONNumber,
+	}
+
+	if !reflect.DeepEqual(sht[0], &s1) {
+		t.Fatalf("failed test expected: %v\n got: %v", s1, *sht[0])
+	}
+
+	s2 := StockHistory{
+		ID:             json.Number("2"),
+		UpdDateTime:    "2018-07-22 11:46:43",
+		TargetDateTime: "2018-07-22 11:46:43",
+		ProductID:      json.Number("8"),
+		StoreID:        json.Number("1"),
+		Amount:         json.Number("1"),
+		StockAmount:    json.Number("1"),
+		StockDivision:  "01",
+		FromStoreID:    &emptyJSONNumber,
+		ToStoreID:      &emptyJSONNumber,
+	}
+
+	if !reflect.DeepEqual(sht[1], &s2) {
+		t.Fatalf("failed test expected: %v\n got: %v", s2, *sht[1])
 	}
 }
